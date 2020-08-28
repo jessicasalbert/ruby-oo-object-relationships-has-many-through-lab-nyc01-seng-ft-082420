07bd0cadc2dbdb450d0efb86dc0bdcef2475c9f4
jessicasalbert <jessicarsalbert@gmail.com>
2020-08-27 21:09:45 -0400

    completed labs

---
+++ lib/appointment.rb
@@ -0,0 +1,22 @@
+class Appointment
+require 'pry'
+    
+    
+    attr_reader :date, :patient, :doctor
+
+    @@all = []
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    #binding.pry
+
+end
